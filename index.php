9e6f553aa49d4cef9e649925c76bb8056c599278
biplab46 <86226067+biplab46@users.noreply.github.com>
2021-06-21 14:40:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,16 @@ include('includes/config.php');
 <!DOCTYPE>
 <html lang="en">
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-199948325-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-199948325-1');
+</script>
+
     <title>
         login
     </title>
